1e359f8a317edf2799303710494886d627a0c354
algorist <algorist@algorist-HP.(none)>
2015-10-07 04:36:19 +0530

    Adding personal cpp template

---
+++ template.cpp
@@ -0,0 +1,40 @@
+/*	
+	Problem link : 
+	Category :
+	f1zz0_13
+*/
+
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <cstdlib>
+#include <cmath>
+#include <ctype.h>
+#include <stack>
+#include <queue>
+#include <limits.h>
+	
+#define rep(i, a) for(int i = 0; i < a; i++)
+#define fo(i, a, b) for(int i = a; i < b; i++)
+#define defo(i, a, b) for(int i = a; i >= b; i--)
+#define ll long long
+#define MAX 1000000007
+#define pb  push_back
+#define mp  make_pair
+#define sz(a)   ((int)(a.size()))
+#define x first
+#define y second
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<double> VD;
+typedef vector<char> VC;
+typedef pair<int, int> PI;
+
+int main() {
+	return 0;
+}
